9ddfaa281d252fd4f85a2e7558490f15e1052e46
nova togatorop <nova@roadbeats.com>
2019-11-12 18:44:50 +0100

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # get 'tasks', to: 'tasks#index', as: :tasks
+  # get 'tasks/new', to: 'tasks#new', as: :tasks_new
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :tasks_edit
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
